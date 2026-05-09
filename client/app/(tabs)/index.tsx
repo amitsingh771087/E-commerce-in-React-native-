92cf56af26a78362e32ecf50cc56615e9492da59
amitsingh771087 <amitsingh77108792@gmail.com>
2026-05-09 09:29:32 +0530

    update miror fix

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -69,6 +69,7 @@ const Home = () => {
                   className="w-full h-full"
                   resizeMode="cover"
                 />
+                <View className="absolute inset-0 bg-black/40" />
                 <View className="absolute bottom-4 left-4 z-10">
                   <Text className="text-white text-2xl font-bold">
                     {banner.title}
@@ -81,7 +82,6 @@ const Home = () => {
                     <Ionicons name="arrow-forward" size={20} color={"black"} />
                   </TouchableOpacity>
                 </View>
-                <View className="absolute inset-0 bg-black/40" />
               </View>
             ))}
           </ScrollView>
