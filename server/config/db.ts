32735591ecab9df446d07045abae7f75a82adc25
amitsingh771087 <amitsingh77108792@gmail.com>
2026-05-13 11:23:24 +0530

    backend setup completed

---
+++ server/config/db.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const mongourl = process.env.MONGODB_URI as string;
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("MongoDB Connected  ");
+    });
+
+    await mongoose.connect(process.env.MONGODB_URI as string);
+  } catch (error) {
+    console.log("MongoDB Connection Failed", error);
+  }
+};
+
+export default connectDB;
